28640a3aa851ec81c2ca1154067f1e112c084881
hardik <hardik.radicalloop@gmail.com>
2023-05-03 09:45:47 +0530

    loader add in download button

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -70,7 +70,30 @@ function Dashboard() {
                   <button className="btn-outline-circle">Open</button>
                 </Link>
               ) : (
-                <button className="btn-outline-circle"> Downloading... {premChatPercentage}% </button>
+                <button className="btn-outline-circle flex items-center">
+                  {" "}
+                  Downloading... {premChatPercentage}%
+                  <svg
+                    className="animate-spin ml-3 h-5 w-5 text-white"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <circle
+                      className="opacity-25"
+                      cx="12"
+                      cy="12"
+                      r="10"
+                      stroke="#F49E97"
+                      stroke-width="4"
+                    ></circle>
+                    <path
+                      className="opacity-75"
+                      fill="#F49E97"
+                      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                    ></path>
+                  </svg>
+                </button>
               )
             ) : (
               <button
