dbeb11284c16624d137ca696d6239ae18382fd86
hardik <hardik.radicalloop@gmail.com>
2023-05-05 19:20:33 +0530

    dropdown desing add

--- src/components/prem-chat/RightSidebar.tsx
+++ src/components/prem-chat/RightSidebar.tsx
@@ -1,9 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import leftArrow from "../../assets/images/left-arrow.svg";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
 
-const RightSidebar = ({setRightSidebar}: any) => {
+const RightSidebar = ({ setRightSidebar }: any) => {
+  const [isActive, setIsActive] = useState(false);
+  const [selected, setIsSelected] = useState("k50");
+
   return (
     <div className="bg-darkgunmetal flex-col px-4 flex h-screen w-[260px]">
       <div className="sidebar-toggle__btn !mt-5 !ml-3">
@@ -20,24 +23,64 @@ const RightSidebar = ({setRightSidebar}: any) => {
         <p className="text-spanishgray text-base font-proximaNova-regular mb-[18px]">
           Preset
         </p>
-        <p className="flex items-center justify-between text-white text-base font-proximaNova-regular">
-          k50
-          <svg
-            width="7"
-            height="10"
-            viewBox="0 0 7 10"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+
+        <div className="dropdown">
+          <div
+            onClick={(e) => {
+              setIsActive(!isActive);
+            }}
+            className="text-white text-base font-proximaNova-regular flex items-center justify-between"
           >
-            <path
-              d="M1 9.25L5.125 5.125L1 1"
-              stroke="white"
-              strokeWidth="1.76786"
-            />
-          </svg>
-        </p>
+            {selected}
+            <svg
+              width="7"
+              height="10"
+              viewBox="0 0 7 10"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M1 9.25L5.125 5.125L1 1"
+                stroke="white"
+                strokeWidth="1.76786"
+              />
+            </svg>
+          </div>
+          <div
+            className="dropdown-content"
+            style={{ display: isActive ? "block" : "none" }}
+          >
+            <div
+              onClick={(e) => {
+                setIsSelected(e.target.textContent);
+                setIsActive(!isActive);
+              }}
+              className="item"
+            >
+              k50
+            </div>
+            <div
+              className="item"
+              onClick={(e) => {
+                setIsSelected(e.target.textContent);
+                setIsActive(!isActive);
+              }}
+            >
+              k50-Creative
+            </div>
+            <div
+              className="item"
+              onClick={(e) => {
+                setIsSelected(e.target.textContent);
+                setIsActive(!isActive);
+              }}
+            >
+              k50-Precise
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="border border-timberwolf -mx-4 opacity-30"/>
+      <div className="border border-timberwolf -mx-4 opacity-30" />
       <ul className="my-[18px] right-sidebar__list overflow-y-auto scrollbar-none">
         <li>
           <p>
@@ -45,7 +88,7 @@ const RightSidebar = ({setRightSidebar}: any) => {
             <span>0.75</span>
           </p>
           <RangeSlider
-            className="single-thumb"
+            // className="single-thumb"
             defaultValue={[0, 50]}
             thumbsDisabled={[true, false]}
             rangeSlideDisabled={true}
