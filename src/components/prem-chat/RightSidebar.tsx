3281da708c0dc999be949e052c55563466817706
Jignesh Solanki <jigneshsolanki92@gmail.com>
2023-05-05 19:05:36 +0530

    integrate right sidebar

--- src/components/prem-chat/RightSidebar.tsx
+++ src/components/prem-chat/RightSidebar.tsx
@@ -2,8 +2,33 @@ import React from "react";
 import leftArrow from "../../assets/images/left-arrow.svg";
 import RangeSlider from "react-range-slider-input";
 import "react-range-slider-input/dist/style.css";
+import usePremChatStore from "../../shared/store/prem-chat";
+import { shallow } from "zustand/shallow";
 
-const RightSidebar = ({setRightSidebar}: any) => {
+const RightSidebar = ({ setRightSidebar }: any) => {
+  const {
+    temperature,
+    setTemperature,
+    max_tokens,
+    setMaxTokens,
+    top_p,
+    setTopP,
+    frequency_penalty,
+    setFrequencyPenalty,
+  } = usePremChatStore(
+    (state) => ({
+      temperature: state.temperature,
+      setTemperature: state.setTemperature,
+      max_tokens: state.max_tokens,
+      setMaxTokens: state.setMaxTokens,
+      top_p: state.top_p,
+      setTopP: state.setTopP,
+      frequency_penalty: state.frequency_penalty,
+      setFrequencyPenalty: state.setFrequencyPenalty,
+    }),
+    shallow
+  );
+
   return (
     <div className="bg-darkgunmetal flex-col px-4 flex h-screen w-[260px]">
       <div className="sidebar-toggle__btn !mt-5 !ml-3">
@@ -37,54 +62,70 @@ const RightSidebar = ({setRightSidebar}: any) => {
           </svg>
         </p>
       </div>
-      <div className="border border-timberwolf -mx-4 opacity-30"/>
+      <div className="border border-timberwolf -mx-4 opacity-30" />
       <ul className="my-[18px] right-sidebar__list overflow-y-auto scrollbar-none">
         <li>
           <p>
             <span>Temperature</span>
-            <span>0.75</span>
+            <span>{temperature}</span>
           </p>
           <RangeSlider
             className="single-thumb"
-            defaultValue={[0, 50]}
+            value={[0, temperature]}
             thumbsDisabled={[true, false]}
             rangeSlideDisabled={true}
+            min={0}
+            max={2}
+            step={0.1}
+            onInput={(value: number[]) => setTemperature(value[1])}
           />
         </li>
         <li>
           <p>
             <span>Max new tokens</span>
-            <span>0.75</span>
+            <span>{max_tokens}</span>
           </p>
           <RangeSlider
             className="single-thumb"
-            defaultValue={[0, 50]}
+            value={[0, max_tokens]}
             thumbsDisabled={[true, false]}
             rangeSlideDisabled={true}
+            min={10}
+            max={2000}
+            step={10}
+            onInput={(value: number[]) => setMaxTokens(value[1])}
           />
         </li>
         <li>
           <p>
             <span>Top P</span>
-            <span>0.75</span>
+            <span>{top_p}</span>
           </p>
           <RangeSlider
             className="single-thumb"
-            defaultValue={[0, 50]}
+            value={[0, top_p]}
             thumbsDisabled={[true, false]}
             rangeSlideDisabled={true}
+            min={0}
+            max={2}
+            step={0.1}
+            onInput={(value: number[]) => setTopP(value[1])}
           />
         </li>
         <li>
           <p>
             <span>Repetition Penalty</span>
-            <span>0.75</span>
+            <span>{frequency_penalty}</span>
           </p>
           <RangeSlider
             className="single-thumb"
-            defaultValue={[0, 50]}
+            value={[-2, frequency_penalty]}
             thumbsDisabled={[true, false]}
             rangeSlideDisabled={true}
+            min={-2}
+            max={2}
+            step={0.1}
+            onInput={(value: number[]) => setFrequencyPenalty(value[1])}
           />
         </li>
         <li>
