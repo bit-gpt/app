da2aa712b5b9dec7a67e7c09db85827332a73712
Jignesh Solanki <jignesh.radicalloop@gmail.com>
2023-05-24 11:28:57 +0530

    filter running services on dashboard

--- src/modules/dashboard/components/ServicesRunning.tsx
+++ src/modules/dashboard/components/ServicesRunning.tsx
@@ -1,21 +1,40 @@
 import ServicesCard from "shared/components/ServicesCard";
 import chatLogo from "assets/images/chat.svg";
+import useServices from "shared/hooks/useServices";
 
 const ServicesRunning = () => {
+  const { data: response, isLoading } = useServices();
+
+  const services = response?.data || [];
+
+  const runningServices = services.filter((service) => service.running);
+
   return (
     <>
       <div className="mask-heading mb-[29px]">
         <h2>Services Running</h2>
       </div>
-      <ServicesCard
-        icon={chatLogo}
-        className="dashboard-bottom__card flex-wrap !pr-5 services-running"
-        title="Vicuna 7B 4-Bit"
-        isRunning={true}
-        onDelete={() => console.log("onDelete")}
-        onStart={() => console.log("onStart")}
-        onStop={() => console.log("onStop")}
-      />
+      {isLoading && (
+        <div className="flex">
+          <label className="text-[#8C8C8C]">Loading ..</label>
+        </div>
+      )}
+      {runningServices.length > 0 && (
+        <ServicesCard
+          icon={chatLogo}
+          className="dashboard-bottom__card flex-wrap !pr-5 services-running"
+          title="Vicuna 7B 4-Bit"
+          isRunning={true}
+          onDelete={() => console.log("onDelete")}
+          onStart={() => console.log("onStart")}
+          onStop={() => console.log("onStop")}
+        />
+      )}
+      {
+        !isLoading && runningServices.length === 0 && (
+          <div className="text-[#8C8C8C]">No services found</div>
+        )
+      }
     </>
   );
 };
