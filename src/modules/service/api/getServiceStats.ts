faafd91c58b27793f0992c62727ceafdb6172b08
Marco Argentieri <3596602+tiero@users.noreply.github.com>
2023-05-28 02:10:23 +0200

    fix: remove api/ prefix (#25)

--- src/modules/service/api/getServiceStats.ts
+++ src/modules/service/api/getServiceStats.ts
@@ -3,6 +3,6 @@ import api from "shared/api/v1";
 import { ServiceStats } from "../types";
 
 const getServiceStats = async (id: string): Promise<AxiosResponse<ServiceStats>> =>
-  api.get(`api/v1/stats/${id}`);
+  api.get(`v1/stats/${id}`);
 
 export default getServiceStats;
