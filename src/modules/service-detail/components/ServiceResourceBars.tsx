30b3db1e671e4196a9fe33cea02d4f2bf1a10c6e
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-05-29 22:08:10 +0200

    fixed stats service

--- src/modules/service-detail/components/ServiceResourceBars.tsx
+++ src/modules/service-detail/components/ServiceResourceBars.tsx
@@ -4,44 +4,38 @@ import { ServiceResourceBarsProps } from "../types";
 const ServiceResourceBars = ({ serviceId }: ServiceResourceBarsProps) => {
   const { data: response } = useServiceStats(serviceId);
 
-  const memory = response?.data?.memory_percentage || 0;
-  const memoryPercent = memory * 100;
-
-  const cpu = response?.data?.cpu_percentage || 0;
-  const cpuPercentage = cpu * 100;
-
   return (
     <ul className="grid xl:grid-cols-3 items-end gap-4 right-top-card card !ml-0 p-5">
       <li>
-        <p>Used Memory/Memory Limit</p>
+        <p>Memory</p>
         <div className="flex gap-[6px] items-center">
-          <p className="!mb-0">{memoryPercent}%</p>
+          <p className="!mb-0">{response?.data?.memory_usage} GiB</p>
           <div className="progress">
             <div
               className="progress-container"
-              style={{ width: `${memoryPercent}%` }}
+              style={{ width: `${response?.data?.memory_percentage}%` }}
             ></div>
           </div>
         </div>
       </li>
       <li>
-        <p>Used CPU</p>
+        <p>CPU</p>
         <div className="flex gap-[6px] items-center">
-          <p className="!mb-0">{cpuPercentage}%</p>
+          <p className="!mb-0">{response?.data?.cpu_percentage}%</p>
           <div className="progress">
             <div
               className="progress-container"
-              style={{ width: `${cpuPercentage}%` }}
+              style={{ width: `${response?.data?.cpu_percentage}%` }}
             ></div>
           </div>
         </div>
       </li>
       <li>
-        <p>Network IO</p>
+        <p>Image Size</p>
         <div className="flex gap-[6px] items-center">
-          <p className="!mb-0">30%</p>
+          <p className="!mb-0">{response?.data?.storage_usage} GiB</p>
           <div className="progress">
-            <div className="progress-container" style={{ width: "30%" }}></div>
+            <div className="progress-container" style={{ width: `${response?.data?.storage_percentage}%` }}></div>
           </div>
         </div>
       </li>
