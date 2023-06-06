e384d7588339d15a3e299d6f6bb9f7730d86af61
Filippo Pedrazzini <filippo.pedrazzini@joandko.io>
2023-06-06 16:50:46 +0200

    added server url logic

--- src/modules/prem-chat/api/getChatCompletion.ts
+++ src/modules/prem-chat/api/getChatCompletion.ts
@@ -1,12 +1,12 @@
 import api from "shared/api/v1";
 import { ChatCompletionInputData } from "../types";
-import { getBackendUrlFromStore } from "shared/store/setting";
+import { getServerUrl } from "shared/helpers/utils";
 
 const getChatCompletion = async (port: number, data: ChatCompletionInputData) => {
-  const backendUrl = new URL(getBackendUrlFromStore());
-  backendUrl.port = `${port}`;
+  const serverUrl = new URL(getServerUrl());
+  serverUrl.port = `${port}`;
 
-  return api().post(`${backendUrl}api/v1/chat/completions`, data);
+  return api().post(`${serverUrl}api/v1/chat/completions`, data);
 };
 
 export default getChatCompletion;
