71c52402fefd84dad73d3bafa9339e64302726a1
hardik <hardik.radicalloop@gmail.com>
2023-06-07 15:59:35 +0530

    mobile handle css

--- src/modules/settings/components/GPUResources.tsx
+++ src/modules/settings/components/GPUResources.tsx
@@ -6,7 +6,7 @@ const GPUResources = () => {
   return (
     <div className="mt-[68px]">
       <h2 className="text-brightgray mb-[37px] text-lg">GPUs</h2>
-      <div className="grid grid-cols-3">
+      <div className="grid lg:grid-cols-3 sm:grid-cols-2 gap-4">
         {response?.data?.gpu_name ? (
           <div className="shadow-default">
             <div className="gpu-resources__header">
