6c494605530e709bb7709b5bf83a09529ccaf32c
hardik <hardik.radicalloop@gmail.com>
2023-06-08 18:38:04 +0530

    markdown css fix

--- src/shared/components/BotReply.tsx
+++ src/shared/components/BotReply.tsx
@@ -7,10 +7,12 @@ import Markdown from "./Markdown";
 const BotReply = ({ reply }: BotReplyProps) => {
   return (
     <div className="bot-reply">
-      <span>
+      <span className="bot-reply__icon mr-3">
         <img className="mx-[22px]" src={chatReplay} alt="chatReplay" width={18} height={18} />
       </span>
-      <Markdown>{reply}</Markdown>
+      <p>
+        <Markdown>{reply}</Markdown>
+      </p>
       <CopyToClipboard text={reply}>
         <button className="ml-3 bg-white mt-auto rounded-[12px] h-[38px] min-w-[35px]">
           <img className="mx-auto" src={copy} alt="copy" width={16} height={16} />
