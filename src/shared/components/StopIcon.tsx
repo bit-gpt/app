1e094facf2663bb472063fdf2eeb3499f6705cab
hardik <hardik.radicalloop@gmail.com>
2023-06-10 13:42:57 +0530

    css fix

--- src/shared/components/StopIcon.tsx
+++ src/shared/components/StopIcon.tsx
@@ -1,6 +1,6 @@
 const StopIcon = () => {
   return (
-    <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <svg width="24" height="24" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
       <circle cx="11.4136" cy="11.647" r="10.0352" stroke="#EDEDED" strokeWidth="1.88161" />
       <rect x="8" y="8" width="7" height="7" fill="#D9D9D9" />
     </svg>
