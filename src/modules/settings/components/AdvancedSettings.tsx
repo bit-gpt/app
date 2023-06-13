555821703c553235a325a508ae9c73c160f10ffe
hardik <hardik.radicalloop@gmail.com>
2023-06-13 19:21:16 +0530

    mobile desing fix

--- src/modules/settings/components/AdvancedSettings.tsx
+++ src/modules/settings/components/AdvancedSettings.tsx
@@ -44,28 +44,42 @@ const AdvancedSettings = () => {
 
   return (
     <div className="md:mb-14 mb-8">
-      <h2 className="!mt-10 text-brightgray mb-[37px] text-lg">Advanced</h2>
+      <h2 className="md:!mt-10 mt-[60px] text-brightgray md:mb-[37px] mb-4 md:text-lg text-sm">
+        Advanced
+      </h2>
       <form onSubmit={onUpdate}>
-        <div className="grid md:grid-cols-2">
-          <label className="text-white mr-2 max-md:mb-4">Backend URL</label>
-          <div className="flex">
+        <div className="flex">
+          <label className="text-white mr-2 max-md:mb-4 backend-url md:text-lg text-[10px]">
+            Backend URL
+          </label>
+          <div className="flex w-full">
             <div className="text-right w-full">
-              <input className="form-control" type="url" value={backendUrl} onChange={onChange} />
+              <input
+                className="form-control max-md:!py-2 max-md:text-[10px]"
+                type="url"
+                value={backendUrl}
+                onChange={onChange}
+              />
               <PrimaryButton
-                className="!px-9 mt-[44px] opacity-70"
+                className="!px-9 mt-[44px] max-md:text-sm max-md:absolute left-0 right-0 bottom-0"
                 type="submit"
                 disabled={isLoading}
               >
                 {isLoading ? "Updating..." : "Update"}
               </PrimaryButton>
             </div>
-            <button id="reset" className="w-[50px] h-[45px]" type="button" onClick={onReset}>
+            <button
+              id="reset"
+              className="w-[50px] h-[45px] max-md:hidden"
+              type="button"
+              onClick={onReset}
+            >
               <ResetIcon />
             </button>
             <Tooltip
               anchorSelect="#reset"
               place="bottom"
-              className="reset-topltip"
+              className="reset-topltip !text-[10px]"
               classNameArrow="reset-topltip__arrow"
             >
               Reset to Default
