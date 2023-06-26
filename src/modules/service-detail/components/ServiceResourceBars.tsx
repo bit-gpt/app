348ba1ca7d9c7bba73dec962436d72303228f263
hardik <hardik.radicalloop@gmail.com>
2023-06-26 16:40:21 +0530

    service detail page css fix

--- src/modules/service-detail/components/ServiceResourceBars.tsx
+++ src/modules/service-detail/components/ServiceResourceBars.tsx
@@ -17,7 +17,7 @@ const ServiceResourceBars = ({ serviceId, status }: ServiceResourceBarsProps) =>
         <li>
           <p>Memory</p>
           <div className="flex gap-[6px] items-center">
-            <p className="!mb-0 whitespace-nowrap">{response?.data?.memory_usage} GiB</p>
+            <p className="!mb-0 whitespace-nowrap"><span className="lg:hidden">-</span> {response?.data?.memory_usage} GiB</p>
             <div className="progress">
               <div
                 className="progress-container"
@@ -29,7 +29,7 @@ const ServiceResourceBars = ({ serviceId, status }: ServiceResourceBarsProps) =>
         <li>
           <p>CPU</p>
           <div className="flex gap-[6px] items-center">
-            <p className="!mb-0 whitespace-nowrap">{response?.data?.cpu_percentage}%</p>
+            <p className="!mb-0 whitespace-nowrap"><span className="lg:hidden">-</span> {response?.data?.cpu_percentage}%</p>
             <div className="progress">
               <div
                 className="progress-container"
@@ -41,7 +41,7 @@ const ServiceResourceBars = ({ serviceId, status }: ServiceResourceBarsProps) =>
         <li>
           <p>Image Size</p>
           <div className="flex gap-[6px] items-center">
-            <p className="!mb-0 whitespace-nowrap">{response?.data?.storage_usage} GiB</p>
+            <p className="!mb-0 whitespace-nowrap"><span className="lg:hidden">-</span> {response?.data?.storage_usage} GiB</p>
             <div className="progress">
               <div
                 className="progress-container"
@@ -52,7 +52,7 @@ const ServiceResourceBars = ({ serviceId, status }: ServiceResourceBarsProps) =>
         </li>
       </ul>
       {status !== "running" && (
-        <p className="flex items-center pb-3 mx-4 gap-[7px] !text-[12px] service-is__not-running">
+        <p className="flex items-center pb-3 md:mx-4 max-md:mt-4 gap-[7px] !text-[12px] service-is__not-running">
           <WarningIcon />
           The service is not Running. Resources not available
         </p>
