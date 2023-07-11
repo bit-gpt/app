873c4a5cd1d5ec573f8b8b7ba7f09389861f591d
hardik <hardik.radicalloop@gmail.com>
2023-07-11 12:45:46 +0530

    dropdown conditional rendering

--- src/modules/service/components/ServiceActions.tsx
+++ src/modules/service/components/ServiceActions.tsx
@@ -10,6 +10,10 @@ import NeedsUpdateServiceState from "./NeedsUpdateServiceState";
 import { round } from "lodash";
 import { AUDIO_TO_TEXT_ID, CHAT_ID, DIFFUSER_ID } from "shared/helpers/utils";
 import ServiceDropdown from "./ServiceDropdown";
+import cross from "assets/images/dot.svg";
+import clsx from "clsx";
+import useStopService from "shared/hooks/useStopService";
+import { toast } from "react-toastify";
 
 const ServiceActions = ({
   status,
@@ -23,6 +27,7 @@ const ServiceActions = ({
 }: ServiceActionsProps) => {
   const navigate = useNavigate();
   const [modalIsOpen, setIsOpen] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
   const onOpenClick = useCallback(() => {
     const isPlayground = interfaces.some((app) => app.playground);
     if (isPlayground) {
@@ -37,15 +42,36 @@ const ServiceActions = ({
     }
     setIsOpen(true);
   }, []);
+  const { mutate, isLoading } = useStopService();
+  const onStop = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    mutate(serviceId, {
+      onSuccess: () => {
+        refetch();
+        toast.success("Service stopped successfully");
+      },
+      onError: () => {
+        toast.error("Failed to stop service");
+      },
+    });
+  };
 
   const documentation = `${interfaces[0]?.documentation || ""}`.trim();
 
   const memoryInGib = round(memoryRequirements / 1024, 2);
 
+  const isInAccessible = [
+    "not_supported",
+    "not_enough_memory",
+    "not_enough_system_memory",
+    "coming_soon",
+  ].includes(status);
+
   return (
     <>
       <div className="flex flex-wrap items-center gap-4 ml-auto">
         {children}
+
         {isDetailView && needsUpdate && (
           <NeedsUpdateServiceState serviceId={serviceId} refetch={refetch} />
         )}
@@ -69,10 +95,29 @@ const ServiceActions = ({
         {status === "not_downloaded" && (
           <NotDownloadedServiceState serviceId={serviceId} refetch={refetch} />
         )}
-        {["not_supported", "not_enough_memory", "not_enough_system_memory", "coming_soon"].includes(
-          status
-        ) && <WarningServiceState status={status} memoryRequirements={memoryInGib} />}
-        <ServiceDropdown />
+
+        {isInAccessible && <WarningServiceState status={status} memoryRequirements={memoryInGib} />}
+
+        {isDetailView && !isInAccessible && status !== "not_downloaded" && (
+          <>
+            <button
+              className={clsx({ "red-badge": needsUpdate })}
+              onClick={() => setDropdownOpen(!dropdownOpen)}
+            >
+              <img src={cross} alt="cross" width={22} height={22} className="w-6 h-6" />
+            </button>
+            <ul className={clsx("dropdown-menu", { "dropdown-active": dropdownOpen })}>
+              {status === "running" && isDetailView && (
+                <li>
+                  <button onClick={onStop}>Stop</button>{" "}
+                </li>
+              )}
+
+              {status === "stopped" && <li>Delete</li>}
+              {needsUpdate && <li className="red-badge">Update</li>}
+            </ul>
+          </>
+        )}
       </div>
       {modalIsOpen && (
         <DocumentationModal
