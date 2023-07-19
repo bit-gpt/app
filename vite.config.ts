944f9b0f9523905b3f41411344722dbda730b9bf
Jignesh Solanki <jigneshsolanki92@gmail.com>
2023-07-19 11:05:20 +0530

    eslint setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tsconfigPaths from 'vite-tsconfig-paths'
-import { splitVendorChunkPlugin } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths";
+import { splitVendorChunkPlugin } from "vite";
+import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
+  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin(), eslint()],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
