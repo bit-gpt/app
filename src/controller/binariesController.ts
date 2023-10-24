216afdebfec4a83610f0e8d826fce769fa7f3a56
Janaka-Steph <kakobaba1212@protonmail.com>
2023-10-24 12:34:03 +0200

    Do not catch error to propagate

--- src/controller/binariesController.ts
+++ src/controller/binariesController.ts
@@ -6,43 +6,23 @@ import AbstractServiceController from "./abstractServiceController";
 
 class BinariesController extends AbstractServiceController {
   async start(serviceId: string): Promise<void> {
-    console.log(`Starting service ${serviceId}`);
-    try {
-      await invoke("start_service", { serviceId });
-    } catch (e) {
-      console.log(e);
-    }
+    await invoke("start_service", { serviceId });
   }
 
   async restart(serviceId: string): Promise<void> {
-    console.log(`Restarting service ${serviceId}`);
-    try {
-      await this.stop(serviceId);
-      const services: string[] = await invoke("get_running_services");
-      while (!services.includes(serviceId)) {
-        await this.start(serviceId);
-      }
-    } catch (e) {
-      console.log(e);
+    await this.stop(serviceId);
+    const services: string[] = await invoke("get_running_services");
+    while (!services.includes(serviceId)) {
+      await this.start(serviceId);
     }
   }
 
   async stop(serviceId: string): Promise<void> {
-    console.log(`Stopping service ${serviceId}`);
-    try {
-      await invoke("stop_service", { serviceId });
-    } catch (e) {
-      console.log(e);
-    }
+    await invoke("stop_service", { serviceId });
   }
 
   async delete(serviceId: string): Promise<void> {
-    console.log(`Deleting service ${serviceId}`);
-    try {
-      await invoke("delete_service", { serviceId });
-    } catch (e) {
-      console.log(e);
-    }
+    await invoke("delete_service", { serviceId });
   }
 
   async download({
@@ -58,70 +38,37 @@ class BinariesController extends AbstractServiceController {
     weightsFiles?: string[];
     afterSuccess?: () => void;
   }): Promise<void> {
-    console.log(`Downloading service ${serviceId}`);
-    try {
-      await invoke("download_service", {
-        binariesUrl,
-        weightsDirectoryUrl,
-        weightsFiles,
-        serviceId,
-      });
-      afterSuccess?.();
-    } catch (e) {
-      console.log(e);
-    }
+    await invoke("download_service", {
+      binariesUrl,
+      weightsDirectoryUrl,
+      weightsFiles,
+      serviceId,
+    });
+    afterSuccess?.();
   }
 
-  async getService(serviceId: string): Promise<ServiceBinary | undefined> {
-    try {
-      return await invoke<ServiceBinary>("get_service_by_id", { serviceId });
-    } catch (e) {
-      console.log(e);
-    }
+  async getService(serviceId: string): Promise<ServiceBinary> {
+    return await invoke<ServiceBinary>("get_service_by_id", { serviceId });
   }
 
-  async getServices(): Promise<ServiceBinary[] | undefined> {
-    try {
-      return await invoke<ServiceBinary[]>("get_services");
-    } catch (e) {
-      console.log(e);
-    }
+  async getServices(): Promise<ServiceBinary[]> {
+    return await invoke<ServiceBinary[]>("get_services");
   }
 
   async getLogs(serviceId: string): Promise<string> {
-    try {
-      return await invoke<string>("get_logs_for_service", { serviceId });
-    } catch (e) {
-      console.log(e);
-      return "";
-    }
+    return await invoke<string>("get_logs_for_service", { serviceId });
   }
 
   async getGPUStats(): Promise<Record<string, string>> {
-    try {
-      return await invoke<Record<string, string>>("get_gpu_stats");
-    } catch (e) {
-      console.log(e);
-      return {};
-    }
+    return await invoke<Record<string, string>>("get_gpu_stats");
   }
 
   async getServiceStats(serviceId: string): Promise<Record<string, string>> {
-    try {
-      return await invoke<Record<string, string>>("get_service_stats", { serviceId });
-    } catch (e) {
-      console.log(e);
-      return {};
-    }
+    return await invoke<Record<string, string>>("get_service_stats", { serviceId });
   }
 
   async getSystemStats(): Promise<Record<string, string>> {
-    try {
-      return await invoke<Record<string, string>>("get_system_stats");
-    } catch (e) {
-      console.log(e);
-      return {};
-    }
+    return await invoke<Record<string, string>>("get_system_stats");
   }
 }
 
