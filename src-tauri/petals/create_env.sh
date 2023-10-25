d0e2268184510b07a8cf9ab6baae65d5ce2218a8
Casper da Costa-Luis <casper.dcl@physics.org>
2023-10-25 10:48:30 +0100

    use miniforge, more tidy

--- src-tauri/petals/create_env.sh
+++ src-tauri/petals/create_env.sh
@@ -1,64 +1,33 @@
 #!/usr/bin/env bash
 prem_appdir="${PREM_APPDIR:-.}"
-requirements="${REQUIREMENTS:-requirements.txt}"
+requirements="$(dirname "$0")/requirements.txt"
 conda_prefix="$prem_appdir/miniconda"
 
-install_conda() {
-  arch="$(uname -m)"
-  os="$(uname -s)"
+if test ! -x "$conda_prefix/bin/mamba"; then
+  echo "Conda not found"
+  os="$(uname)"
   case "$os" in
-    Darwin)
-      case "$arch" in
-        x86_64|arm64)
-          ;;
-        *)
-          echo >&2 "Unsupported architecture: $arch"
-          exit 1
-          ;;
-      esac
-      ;;
-    Linux)
-      case "$arch" in
-        x86_64|aarch64|armv7l)
-          ;;
-        *)
-          echo >&2 "Unsupported architecture: $arch"
-          exit 1
-          ;;
-      esac
+    MacOSX|Linux)
       ;;
     *)
       echo >&2 "Unsupported operating system: $os"
       exit 1
       ;;
   esac
+  arch="$(uname -m)"
 
   mkdir -p "$conda_prefix"
-  os="$(uname -s)"
-  case "$os" in
-    Darwin)
-      os_slug="MacOSX"
-      ;;
-    Linux)
-      os_slug="Linux"
-      ;;
-    *)
-      echo >&2 "Unsupported operating system: $os"
-      exit 1
-      ;;
-  esac
-  echo "Downloading Conda installer for $os_slug-$arch"
-  curl -fsSL "https://repo.anaconda.com/miniconda/Miniconda3-latest-$os_slug-$arch.sh" --output "$prem_appdir/conda_install.sh"
+  echo "Downloading Conda installer for $os-$arch"
+  curl -fsSL "https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-$os-$arch.sh" --output "$prem_appdir/conda_install.sh"
   echo "Installing Conda at '$conda_prefix'"
   sh "$prem_appdir/conda_install.sh" -b -u -p "$conda_prefix"
-}
+fi
 
-# install conda if not found
-test -x "$conda_prefix/bin/conda" || install_conda
+echo "Using Conda at '$conda_prefix'"
+export PREM_PYTHON="$conda_prefix/envs/prem_app/bin/python"
 
-# ensure prem_app env exists
-export PREM_PYTHON="conda_prefix/envs/prem_app/bin/python"
-test -x "$PREM_PYTHON" || "$conda_prefix/bin/conda" create -n prem_app python=3.11 -y
+echo "Ensuring env 'prem_app' exists"
+test -x "$PREM_PYTHON" || "$conda_prefix/bin/mamba" create -y -n prem_app python=3.11
 
-# install requirements
+echo "Installing requirements"
 "$PREM_PYTHON" -m pip install -r "$requirements"
