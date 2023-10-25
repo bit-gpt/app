4bbf2d357dbbb2ccd3f610f636df1fd2f3f93d0a
Janaka-Steph <kakobaba1212@protonmail.com>
2023-10-25 17:21:23 +0200

    Move React Query button

--- src/main.tsx
+++ src/main.tsx
@@ -19,7 +19,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
     </QueryClientProvider>
   </React.StrictMode>,
 );
