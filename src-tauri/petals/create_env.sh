6bf39ef034864429b9a4dec2bfae5ccc9d1ad0ac
Casper da Costa-Luis <casper.dcl@physics.org>
2023-10-25 10:23:42 +0100

    tidy miniconda installation script

--- src-tauri/petals/create_env.sh
+++ src-tauri/petals/create_env.sh
@@ -1,63 +1,63 @@
-# Define the Miniconda installation path and environment name
-miniconda_path=${PREM_APPDIR:-.}
-conda_env=${PREM_PYTHON:-python}
-requirements=${REQUIREMENTS:-requirements.txt}
+#!/usr/bin/env bash
+prem_appdir="${PREM_APPDIR:-.}"
+requirements="${REQUIREMENTS:-requirements.txt}"
+conda_prefix="$prem_appdir/miniconda"
 
-# Function to install Miniconda if not already present
-install_miniconda() {
-    local arch
+# install conda if not found
+install_conda() {
+  arch="$(uname -m)"
+  os="$(uname -s)"
+  case "$os" in
+    Darwin)
+      case "$arch" in
+        x86_64|arm64)
+          ;;
+        *)
+          echo >&2 "Unsupported architecture: $arch"
+          exit 1
+          ;;
+      esac
+      ;;
+    Linux)
+      case "$arch" in
+        x86_64|aarch64|armv7l)
+          ;;
+        *)
+          echo >&2 "Unsupported architecture: $arch"
+          exit 1
+          ;;
+      esac
+      ;;
+    *)
+      echo >&2 "Unsupported operating system: $os"
+      exit 1
+      ;;
+  esac
 
-    if [ "$(uname -s)" == "Darwin" ]; then
-        # macOS
-        if [ "$1" == "x86_64" ]; then
-            arch="x86_64"
-        elif [ "$1" == "arm64" ]; then
-            arch="arm64"
-        else
-            echo "Unsupported architecture: $1"
-            exit 1
-        fi
-    elif [ "$(uname -s)" == "Linux" ]; then
-        # Linux
-        if [ "$1" == "x86_64" ]; then
-            arch="x86_64"
-        elif [ "$1" == "aarch64" ]; then
-            arch="aarch64"
-        elif [ "$1" == "armv7l" ]; then
-            arch="armv7l"
-        else
-            echo "Unsupported architecture: $1"
-            exit 1
-        fi
-    else
-        echo "Unsupported operating system: $(uname -s)"
-        exit 1
-    fi
-
-    # Create the 'appdir' directory if it doesn't exist
-    if [ ! -d "$miniconda_path" ]; then
-        mkdir "$miniconda_path"
-
-        # Download and install Miniconda based on OS and architecture
-        if [ "$(uname -s)" == "Darwin" ]; then
-            wget -nv "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-$arch.sh" -O "$miniconda_path/miniconda_installer.sh"
-        elif [ "$(uname -s)" == "Linux" ]; then
-            wget -nv "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$arch.sh" -O "$miniconda_path/miniconda_installer.sh"
-        fi
-
-        bash "$miniconda_path/miniconda_installer.sh" -b -u -p "$miniconda_path"
-    else
-        echo "Using existing Miniconda in '$miniconda_path'."
-    fi
+  if test ! -d "$conda_prefix"; then
+    mkdir -p "$conda_prefix"
+    # download installer
+    case "$(uname -s)" in
+      Darwin)
+        wget -nv "https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-$arch.sh" -O "$prem_appdir/conda_install.sh"
+        ;;
+      Linux)
+        wget -nv "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$arch.sh" -O "$prem_appdir/conda_install.sh"
+        ;;
+    esac
+    # install
+    sh "$prem_appdir/conda_install.sh" -b -u -p "$conda_prefix"
+  else
+    echo "Using existing Conda in '$conda_prefix'"
+  fi
 }
 
-# Check if Miniconda is already installed
-if ! command -v "$miniconda_path/bin/conda" &>/dev/null; then
-    install_miniconda "$(uname -m)"
-fi
+# install conda if not found
+test -x "$conda_prefix/bin/conda" || install_conda
 
-# Check if the Miniconda environment 'prem_env' exists
-if [ ! -d "$miniconda_path/envs/$conda_env" ]; then
-    "$miniconda_path/bin/conda" create -n "$conda_env" python=3.11 -y
-    "$miniconda_path/envs/$conda_env/bin/pip" install -r "$requirements"
-fi
+# ensure prem_app env exists
+export PREM_PYTHON="conda_prefix/envs/prem_app/bin/python"
+test -x "$PREM_PYTHON" || "$conda_prefix/bin/conda" create -n prem_app python=3.11 -y
+
+# install requirements
+"$PREM_PYTHON" -m pip install -r "$requirements"
