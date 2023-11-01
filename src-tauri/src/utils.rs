f5bc4126b859ba5a3aa5880e07503e7e18f69c2e
tiero <3596602+tiero@users.noreply.github.com>
2023-11-01 18:20:55 +0100

    fix: gracefully handle process kill (#442)

--- src-tauri/src/utils.rs
+++ src-tauri/src/utils.rs
@@ -1,51 +1,60 @@
-use crate::errors::Result;
-use crate::{Service, SharedState};
+use crate::errors::{Context, Result};
+use crate::{err, Service, SharedState};
 use reqwest::get;
 use std::collections::HashMap;
-use tauri::State;
+use std::sync::Arc;
 
-pub async fn fetch_services_manifests(url: &str, state: &State<'_, SharedState>) -> Result<()> {
-    let response = get(url).await.expect("Failed to fetch registry");
-    let services = response.json::<Vec<Service>>().await.unwrap();
+pub async fn fetch_services_manifests(url: &str, state: Arc<SharedState>) -> Result<()> {
+    let response = get(url)
+        .await
+        .with_context(|| format!("Couldn't fetch the manifest from {url:?}"))?;
+    let services = response
+        .json::<Vec<Service>>()
+        .await
+        .with_context(|| "Failed to parse response to list of services")?;
     let mut services_guard = state.services.lock().await;
-    let service_ids = services_guard.keys().cloned().collect::<Vec<String>>();
-    for service in services {
-        if !service_ids.contains(&service.id.clone().unwrap()) {
-            services_guard.insert(service.id.clone().unwrap(), service);
-        }
-    }
+    // TODO: discuss why do we need global ids, why not use uuids and generate them on each load
+    *services_guard = services
+        .into_iter()
+        // removes services without id
+        .filter_map(|x| Some((x.id.clone()?, x)))
+        // removes duplicate services
+        .collect();
     Ok(())
 }
 
-pub fn is_aarch64() -> bool {
+pub const fn is_aarch64() -> bool {
     cfg!(target_arch = "aarch64")
 }
 
-pub fn is_x86_64() -> bool {
+pub const fn is_x86_64() -> bool {
     cfg!(target_arch = "x86_64")
 }
 
+pub const fn is_macos() -> bool {
+    cfg!(target_os = "macos")
+}
+
+pub const fn is_unix() -> bool {
+    cfg!(target_family = "unix")
+}
+
 pub fn get_binary_url(binaries_url: &HashMap<String, Option<String>>) -> Result<String> {
-    let mut binary_url = "".to_string();
-    if is_aarch64() {
-        binary_url = binaries_url
-            .get("aarch64-apple-darwin")
-            .unwrap()
-            .clone()
-            .unwrap_or_else(|| "Unsupported architecture".to_string())
-    } else if is_x86_64() {
-        binary_url = binaries_url
-            .get("x86_64-apple-darwin")
-            .unwrap()
-            .clone()
-            .unwrap_or_else(|| "Unsupported architecture".to_string())
+    if !is_unix() && !is_macos() {
+        err!("Unsupported OS")
+    } else if is_macos() {
+        if is_x86_64() {
+            binaries_url.get("x86_64-apple-darwin")
+        } else if is_aarch64() {
+            binaries_url.get("aarch64-apple-darwin")
+        } else {
+            err!("Unsupported architecture")
+        }
+        .or_else(|| binaries_url.get("universal-apple-darwin"))
+        .with_context(|| "No binary available for platform")?
+        .clone()
+        .with_context(|| "Service not supported on your platform")
+    } else {
+        err!("Unsupported platform")
     }
-    if binary_url == "Unsupported architecture" {
-        binary_url = binaries_url
-            .get("universal-apple-darwin")
-            .unwrap()
-            .clone()
-            .unwrap_or_else(|| Err("Unsupported architecture").unwrap())
-    }
-    Ok(binary_url)
 }
