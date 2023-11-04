56bdb8e4581b862c9a7e274defc55c46567f1d46
tiero <3596602+tiero@users.noreply.github.com>
2023-11-04 19:10:06 +0100

    feat: improve service Card

---
+++ src/modules/service/components/PeerToPeer.tsx
@@ -0,0 +1,7 @@
+import clsx from "clsx";
+
+const PeerToPeer = ({ className }: React.ButtonHTMLAttributes<HTMLButtonElement>) => {
+  return <sup className={clsx("text-[11px] text-white", className)}>🍐 PEER TO PEER</sup>;
+};
+
+export default PeerToPeer;
