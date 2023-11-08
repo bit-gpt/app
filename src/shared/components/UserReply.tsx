973fe7f9ddabab1d455a8d354d3a92a60b83a721
Janaka-Steph <kakobaba1212@protonmail.com>
2023-11-08 13:23:28 +0100

    Chat user prompt preserve formatting (#525)

--- src/shared/components/UserReply.tsx
+++ src/shared/components/UserReply.tsx
@@ -4,7 +4,7 @@ import type { UserReplyProps } from "shared/types";
 const UserReply = ({ reply }: UserReplyProps) => {
   return (
     <div className="user-reply">
-      <p>{reply}</p>
+      <pre>{reply}</pre>
       <div className="flex justify-center align-center bg-grey-100 ml-3 rounded-full min-w-[35px] h-[35px]">
         <img src={user} alt="user" width={16} height={16} />
       </div>
